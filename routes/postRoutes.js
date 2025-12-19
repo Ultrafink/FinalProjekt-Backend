ea55197488abcdd5c884295c4586f5c8e7309010
Ultrafink <finglar@gmail.com>
2025-12-19 01:28:35 +0100

    1

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 import {
   createPost,
   getMyPosts,
